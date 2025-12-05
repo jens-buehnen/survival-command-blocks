467a09831b6915f4acbcccf69955e48ee403e671
fdgdfgdfgfgd <jens.buehnen@outlook.de>
2025-12-05 07:01:26 +0100

    init

---
+++ src/main/java/pan/jensLMAO/scb/PlayerEntityMixin.java
@@ -0,0 +1,26 @@
+package pan.jensLMAO.scb.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerAbilities;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityMixin extends LivingEntity {
+    protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Inject(method = "isCreativeLevelTwoOp", at = @At("HEAD"), cancellable = true)
+    private void isCreativeLevelTwoOp(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+}
